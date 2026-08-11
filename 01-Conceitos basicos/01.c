576d2959a8c58463b880ac3e06febde704c87028
Larissa Almeida <larissavitoria04@gmail.com>
2026-08-11 02:39:37 +0000

    aula1

---
+++ 01-Conceitos basicos/01.c
@@ -0,0 +1,18 @@
+//Atividade assíncrona: escrever um programa que pede a idade do usuário e imprime se é maior ou menor de idade (if/else simples). Conteúdo: funções printf e scanf, estruturas de decisão (if, else, else if, switch), operadores relacionais e lógicos.
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int idade = 0;
+    
+    printf("Digite sua idade:\n");
+    scanf ("%d", &idade);
+    if (idade >= 18){
+    printf ("Maior de idade!");
+        
+    } else {
+        printf("Menor de idade!");
+    }
+
+    return 0;
+}
